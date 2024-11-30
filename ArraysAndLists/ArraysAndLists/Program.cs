7c8b8864257b4f26fc6747a363f6f34e3feb8161
1300Aloce4403 <lskysm@gmail.com>
2024-11-30 06:27:46 +0000

    Comments in the code.

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -7,31 +7,32 @@ namespace ArraysAndLists
     {
         static void Main()
         {
+            // INT ARRAY
+            int[] intArray = { 12, 23, 34, 45, 56 }; //Creating the array of integers.
 
-            int[] intArray = { 12, 23, 34, 45, 56 };
+            Console.WriteLine("Choose a number between 0 and 4!"); //Asking for input of the user.
+            int answer1 = Convert.ToInt32(Console.ReadLine()); //Converting and storing the answer.
+            bool isCorrect = answer1 >= intArray.Length; //Evaluating the answer by comparing it to the length of the array.
 
-            Console.WriteLine("Choose a number between 0 and 4!");
-            int answer1 = Convert.ToInt32(Console.ReadLine());
-            bool isCorrect = answer1 >= intArray.Length;
-
-            if (isCorrect)
+            if (isCorrect) //Branching for exception handling, based on bool value
             {
-                Console.WriteLine("Nope.");
+                Console.WriteLine("Nope."); //Error message if the user's answer is incorrect.
             }
-            else
+            else //In case the answer evaluates to true.
             {
-                Console.WriteLine(intArray[answer1]);
+                Console.WriteLine(intArray[answer1]); //Printing out the value on the index the user requested.
             }
 
-            string[] meats = new string[] { "Chicken", "Chorizo", "Steak" };
+            // STRING ARRAY
+            string[] meats = new string[] { "Chicken", "Chorizo", "Steak" }; //Creating the array of strings.
 
-            Console.WriteLine("\nChoose a number between 0 and 2");
-            int answer2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nChoose a number between 0 and 2"); //Asking for input of the user.
+            int answer2 = Convert.ToInt32(Console.ReadLine()); //Converting and storing the user's answer.
 
-            Console.WriteLine(meats[answer2]);
+            Console.WriteLine(meats[answer2]); //Printing out the value on the index requested by the user.
 
 
-            Console.ReadLine();
+            Console.ReadLine(); //Keeping the window open.
 
         }
     }
