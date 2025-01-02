37dda2d74bff0ab86b8b8e1636ea5b303ad8e6e0
1300Aloce4403 <lskysm@gmail.com>
2025-01-02 17:03:24 +0000

    Commit 1

---
+++ MethodClassAssignment/MethodClassAssignment/ClassOne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class ClassOne
+    {
+        public void Ops(int num1, int num2) //Creating the method that takes two parameters, doing an operation with the first one and displays the second untouched
+        {
+            int result = num1 + 100;
+            Console.WriteLine(result);
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassOne stuff = new ClassOne(); //Instatiate the class
+            stuff.Ops(5, 10); //Call the method by passing in two numbers
+            stuff.Ops(num1: 7, num2: 20); //Call the method, passing the paramethers by specifying their names
+            Console.ReadLine();
+        }
+    }
+}
