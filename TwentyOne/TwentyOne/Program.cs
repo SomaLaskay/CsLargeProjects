6ea7c582f98296ae6e7d70ddf0a7d0fe1c231f2f
1300Aloce4403 <lskysm@gmail.com>
2025-01-02 20:40:52 +0000

    Commit 1

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,6 +10,9 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Dealer = "Soma";
+            game.Name = "TwentyOne";
             Deck deck = new Deck();
             deck.Shuffle(15);
 
