9638ec51ac3f1dfc2d22c34445bf08349d632774
1300Aloce4403 <lskysm@gmail.com>
2025-01-04 11:50:09 +0000

    Operators Assignment Submission

---
+++ EmployeeComparison/EmployeeComparison/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComparison
+{
+    public class Employee
+    {
+        // Property to hold the unique identifier of the employee
+        public int Id { get; set; }
+
+        // Property to hold the first name of the employee
+        public string FirstName { get; set; }
+
+        // Property to hold the last name of the employee
+        public string LastName { get; set; }
+
+        // Overload the '==' operator to compare two Employee objects by their Id
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            // Compare the Id property of both Employee objects
+            return emp1.Id == emp2.Id;
+        }
+
+        // Overload the '!=' operator to compare two Employee objects by their Id
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            // Return the negation of the '==' operator
+            return !(emp1 == emp2);
+        }
+           
+    }
+}
---
+++ EmployeeComparison/EmployeeComparison/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComparison
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two Employee objects with the same Id
+            Employee emp1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee emp2 = new Employee { Id = 5, FirstName = "Jane", LastName = "Smith" };
+            
+            // Compare emp1 and emp2 using the overloaded '==' operator
+            Console.WriteLine($"emp1 == emp2: {emp1 == emp2}"); // Output: True
+            
+            Console.ReadLine();
+        }
+    }
+}
