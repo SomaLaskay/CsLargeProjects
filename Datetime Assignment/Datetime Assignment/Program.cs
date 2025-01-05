ce806a965d4da6107f63f2875466519e7ac7cff3
1300Aloce4403 <lskysm@gmail.com>
2025-01-05 11:34:36 +0000

    Datetime Assignment

---
+++ Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDT = DateTime.Now; //Assigning the current DateTime to a variable
+            Console.WriteLine($"The current Date and Time is {currentDT}."); //Writing out the current DateTime
+            Console.WriteLine("\nHow many hours you need to arrive?"); //Asking the user how many hours they need to arrive
+            int userDelay = Convert.ToInt32(Console.ReadLine()); //Converting and storing the answer into a variable
+            Console.WriteLine($"\nYour ETA is confirmed: {currentDT.AddHours(userDelay)}."); //Writing out the calculated new DateTime as an answer
+            
+            Console.ReadLine();
+        }
+    }
+}
